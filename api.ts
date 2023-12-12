31c0e66080bf8c4b6848ee14a1654da94cd54059
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2023-12-12 18:39:27 +0600

    edit button create and the function working fine

--- api.ts
+++ api.ts
@@ -20,6 +20,18 @@ export const addTodo = async (todo: ITask): Promise<ITask> =>{
   return newTodo;
 }
 
+export const editTodo = async (todo: ITask): Promise<ITask> =>{
+  const res = await fetch(`${baseUrl}/tasks/${todo.id}`,{
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(todo)
+  })
+  const updatedTodo = await res.json();
+  return updatedTodo;
+}
+
 
 
 

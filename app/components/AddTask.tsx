7f15c8dcebc11d9723d8e4fd06740f94a0bc0288
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2023-12-12 18:09:18 +0600

    Complete some new function and debugging some errors

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -2,16 +2,25 @@
 import { AiOutlinePlus } from "react-icons/ai";
 import Modal from "./Modal";
 import { FormEventHandler, useState } from "react";
+import { addTodo } from "@/api";
+import { useRouter } from "next/navigation";
+import { v4 as uuidv4 } from 'uuid';
 
 const AddTask = () => {
+  const router = useRouter();
   const [modalOpen, setModalOpen] = useState<boolean>(false);
-  const [newTaskValue, setNewTaskValue] = useState<string>('');
-  
-  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = (e) =>{
+  const [newTaskValue, setNewTaskValue] = useState<string>("");
+
+  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
-    console.log(newTaskValue);
+    await addTodo({
+      id: uuidv4(),
+      text: newTaskValue,
+    });
     setNewTaskValue("");
-  }
+    setModalOpen(false);
+    router.refresh();
+  };
 
   return (
     <div>
@@ -27,12 +36,14 @@ const AddTask = () => {
           <div className="modal-action">
             <input
               value={newTaskValue}
-              onChange={e => setNewTaskValue(e.target.value)}
+              onChange={(e) => setNewTaskValue(e.target.value)}
               type="text"
               placeholder="Type here"
               className="input input-bordered w-full"
             />
-            <button type="submit" className="btn">Submit</button>
+            <button type="submit" className="btn">
+              Submit
+            </button>
           </div>
         </form>
       </Modal>
